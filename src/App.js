f52967e7ab679b0acc3b15c84c8a811c78fb3258
chadNoliver <chadNoliver@gmail.com>
2023-01-21 15:54:43 -0600

    Modify to test for deployment success

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         This is dev
+         This is dev branch
         </a>
       </header>
     </div>
