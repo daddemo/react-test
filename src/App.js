8a2821a5f1b9c946c263248facf59c91b22c0c7d
chadNoliver <chadNoliver@gmail.com>
2023-01-21 18:29:58 -0600

    differentiate dev

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is dev
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         This is dev branch
-        </a>
+        </div>
       </header>
     </div>
   );
